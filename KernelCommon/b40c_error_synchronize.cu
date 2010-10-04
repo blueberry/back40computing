cc803ebee0834f43cf821af9845a9d68349253e6
duane.merrill <duane.merrill@f28d7702-48ea-1802-4fdf-2413024927d0>
2010-10-04 20:22:02 +0000

    Update for host-kernel-sync error-reporting (by compiler flag)

---
+++ KernelCommon/b40c_error_synchronize.cu
@@ -0,0 +1,46 @@
+/**
+ * Copyright 2010 Duane Merrill
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ * 
+ * For more information, see our Google Code project site: 
+ * http://code.google.com/p/back40computing/
+ * 
+ * Thanks!
+ */
+
+#pragma once
+
+namespace b40c {
+
+void synchronize(const char *message)
+{
+	cudaError_t error = cudaThreadSynchronize();
+	if(error) {
+		fprintf(stderr, "%s\n", message);
+		exit(1);
+	}
+} 
+
+void synchronize_if_enabled(const char *message)
+{
+	// XXX this could potentially be a runtime decision
+#if __ERROR_SYNCHRONOUS
+	synchronize(message);
+#endif
+} 
+
+
+
+} // namespace b40c
+
