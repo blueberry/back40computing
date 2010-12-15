d2deaaecb9628617f1a56b51022f6d74e88ec3b1
duane.merrill <duane.merrill@f28d7702-48ea-1802-4fdf-2413024927d0>
2010-12-15 22:05:09 +0000

    Small updates to helpful texts

--- trunk/makedistro.sh
+++ trunk/makedistro.sh
@@ -4,4 +4,4 @@ export VER=`grep -o v[0-9]*.[0-9]*.[0-9]* VERSION.TXT`
 
 tar --exclude=*/.svn --exclude=*/bin --exclude=*/testlab -czvf B40C_LsbRadixSort.$VER.tgz    LICENSE.TXT VERSION.TXT Common ExamplesInclude LsbRadixSort
 tar --exclude=*/.svn --exclude=*/bin --exclude=*/testlab -czvf B40C_BFS.$VER.tgz             LICENSE.TXT VERSION.TXT Common ExamplesInclude BFS
-tar --exclude=*/.svn --exclude=*/bin --exclude=*/testlab -czvf B40C_Primitives.$VER.tgz      LICENSE.TXT VERSION.TXT Common ExamplesInclude LsbRadixSort BFS
+tar --exclude=*/.svn --exclude=*/bin --exclude=*/testlab -czvf B40C_AllPrimitives.$VER.tgz      LICENSE.TXT VERSION.TXT Common ExamplesInclude LsbRadixSort BFS
