5b9c0887d050c01a2d149385bdd0fb472b72c408
duane.merrill <duane.merrill@f28d7702-48ea-1802-4fdf-2413024927d0>
2010-12-22 17:55:34 +0000

    Added level-grid kernel alternative

--- BFS/kernel/bfs_level_grid_kernel.cu
+++ BFS/kernel/bfs_level_grid_kernel.cu
@@ -23,7 +23,8 @@
 
 /******************************************************************************
  * 
- * BFS-SG: A single-grid breadth-first-search kernel.  
+ * BFS-LEVEL: A level-syncronized breadth-first-search kernel.  (One level per 
+ * launch.)  
  * 
  ******************************************************************************/
 
@@ -34,58 +35,58 @@
 namespace b40c {
 
 /******************************************************************************
- * BFS-SG Granularity Configuration 
+ * BFS-LEVEL Granularity Configuration 
  ******************************************************************************/
 
 //  CTA size in threads
-#define B40C_BFS_SG_SM20_LOG_CTA_THREADS(strategy)				((strategy == EXPAND_CONTRACT) ? 7 : 7)				// 128 threads on GF100		 
-#define B40C_BFS_SG_SM12_LOG_CTA_THREADS(strategy)				((strategy == EXPAND_CONTRACT) ? 8 : 8)		 		// 128 threads on GT200
-#define B40C_BFS_SG_SM10_LOG_CTA_THREADS(strategy)				((strategy == EXPAND_CONTRACT) ? 8 : 8)				// 128 threads on G80
-#define B40C_BFS_SG_LOG_CTA_THREADS(sm_version, strategy)		((sm_version >= 200) ? B40C_BFS_SG_SM20_LOG_CTA_THREADS(strategy) : 	\
-																 (sm_version >= 120) ? B40C_BFS_SG_SM12_LOG_CTA_THREADS(strategy) : 	\
-																					   B40C_BFS_SG_SM10_LOG_CTA_THREADS(strategy))		
+#define B40C_BFS_LEVEL_SM20_LOG_CTA_THREADS(strategy)				((strategy == EXPAND_CONTRACT) ? 7 : 7)				// 128 threads on GF100		 
+#define B40C_BFS_LEVEL_SM12_LOG_CTA_THREADS(strategy)				((strategy == EXPAND_CONTRACT) ? 8 : 8)		 		// 128 threads on GT200
+#define B40C_BFS_LEVEL_SM10_LOG_CTA_THREADS(strategy)				((strategy == EXPAND_CONTRACT) ? 8 : 8)				// 128 threads on G80
+#define B40C_BFS_LEVEL_LOG_CTA_THREADS(sm_version, strategy)		((sm_version >= 200) ? B40C_BFS_LEVEL_SM20_LOG_CTA_THREADS(strategy) : 	\
+																	 (sm_version >= 120) ? B40C_BFS_LEVEL_SM12_LOG_CTA_THREADS(strategy) : 	\
+																					   B40C_BFS_LEVEL_SM10_LOG_CTA_THREADS(strategy))		
 
 // Target CTA occupancy.  Params: SM sm_version
-#define B40C_BFS_SG_SM20_OCCUPANCY()							(8)				// 8 threadblocks on GF100
-#define B40C_BFS_SG_SM12_OCCUPANCY()							(1)				// 1 threadblocks on GT200
-#define B40C_BFS_SG_SM10_OCCUPANCY()							(1)				// 1 threadblocks on G80
-#define B40C_BFS_SG_OCCUPANCY(sm_version)						((sm_version >= 200) ? B40C_BFS_SG_SM20_OCCUPANCY() : 	\
-																 (sm_version >= 120) ? B40C_BFS_SG_SM12_OCCUPANCY() : 	\
-																					   B40C_BFS_SG_SM10_OCCUPANCY())		
+#define B40C_BFS_LEVEL_SM20_OCCUPANCY()							(8)				// 8 threadblocks on GF100
+#define B40C_BFS_LEVEL_SM12_OCCUPANCY()							(1)				// 1 threadblocks on GT200
+#define B40C_BFS_LEVEL_SM10_OCCUPANCY()							(1)				// 1 threadblocks on G80
+#define B40C_BFS_LEVEL_OCCUPANCY(sm_version)					((sm_version >= 200) ? B40C_BFS_LEVEL_SM20_OCCUPANCY() : 	\
+																 (sm_version >= 120) ? B40C_BFS_LEVEL_SM12_OCCUPANCY() : 	\
+																					   B40C_BFS_LEVEL_SM10_OCCUPANCY())		
 
 
 // Vector size of load. Params: SM sm_version, algorithm				
 // (N.B.: currently only vec-1 for EXPAND_CONTRACT, up to vec-4 for CONTRACT_EXPAND)  
-#define B40C_BFS_SG_SM20_LOG_LOAD_VEC_SIZE(strategy)			((strategy == EXPAND_CONTRACT) ? 0 : 0)		 
-#define B40C_BFS_SG_SM12_LOG_LOAD_VEC_SIZE(strategy)			((strategy == EXPAND_CONTRACT) ? 0 : 1)		 
-#define B40C_BFS_SG_SM10_LOG_LOAD_VEC_SIZE(strategy)			((strategy == EXPAND_CONTRACT) ? 0 : 1)		
-#define B40C_BFS_SG_LOG_LOAD_VEC_SIZE(sm_version, strategy)		((sm_version >= 200) ? B40C_BFS_SG_SM20_LOG_LOAD_VEC_SIZE(strategy) : 	\
-																 (sm_version >= 120) ? B40C_BFS_SG_SM12_LOG_LOAD_VEC_SIZE(strategy) : 	\
-																					   B40C_BFS_SG_SM10_LOG_LOAD_VEC_SIZE(strategy))		
+#define B40C_BFS_LEVEL_SM20_LOG_LOAD_VEC_SIZE(strategy)			((strategy == EXPAND_CONTRACT) ? 0 : 0)		 
+#define B40C_BFS_LEVEL_SM12_LOG_LOAD_VEC_SIZE(strategy)			((strategy == EXPAND_CONTRACT) ? 0 : 1)		 
+#define B40C_BFS_LEVEL_SM10_LOG_LOAD_VEC_SIZE(strategy)			((strategy == EXPAND_CONTRACT) ? 0 : 1)		
+#define B40C_BFS_LEVEL_LOG_LOAD_VEC_SIZE(sm_version, strategy)		((sm_version >= 200) ? B40C_BFS_LEVEL_SM20_LOG_LOAD_VEC_SIZE(strategy) : 	\
+																 (sm_version >= 120) ? B40C_BFS_LEVEL_SM12_LOG_LOAD_VEC_SIZE(strategy) : 	\
+																					   B40C_BFS_LEVEL_SM10_LOG_LOAD_VEC_SIZE(strategy))		
 
 
 // Number of raking threads.  Params: SM sm_version, strategy			
 // (N.B: currently supported up to 1 warp)
-#define B40C_BFS_SG_SM20_LOG_RAKING_THREADS()					(B40C_LOG_WARP_THREADS + 0)		// 1 raking warps on GF100
-#define B40C_BFS_SG_SM12_LOG_RAKING_THREADS()					(B40C_LOG_WARP_THREADS + 0)		// 1 raking warps on GT200
-#define B40C_BFS_SG_SM10_LOG_RAKING_THREADS()					(B40C_LOG_WARP_THREADS + 0)		// 1 raking warps on G80
-#define B40C_BFS_SG_LOG_RAKING_THREADS(sm_version, strategy)	((sm_version >= 200) ? B40C_BFS_SG_SM20_LOG_RAKING_THREADS() : 	\
-																 (sm_version >= 120) ? B40C_BFS_SG_SM12_LOG_RAKING_THREADS() : 	\
-																					   B40C_BFS_SG_SM10_LOG_RAKING_THREADS())		
+#define B40C_BFS_LEVEL_SM20_LOG_RAKING_THREADS()					(B40C_LOG_WARP_THREADS + 0)		// 1 raking warps on GF100
+#define B40C_BFS_LEVEL_SM12_LOG_RAKING_THREADS()					(B40C_LOG_WARP_THREADS + 0)		// 1 raking warps on GT200
+#define B40C_BFS_LEVEL_SM10_LOG_RAKING_THREADS()					(B40C_LOG_WARP_THREADS + 0)		// 1 raking warps on G80
+#define B40C_BFS_LEVEL_LOG_RAKING_THREADS(sm_version, strategy)		((sm_version >= 200) ? B40C_BFS_LEVEL_SM20_LOG_RAKING_THREADS() : 	\
+																	 (sm_version >= 120) ? B40C_BFS_LEVEL_SM12_LOG_RAKING_THREADS() : 	\
+																					   B40C_BFS_LEVEL_SM10_LOG_RAKING_THREADS())		
 
 // Size of sractch space (in bytes).  Params: SM sm_version, strategy
-#define B40C_BFS_SG_SM20_SCRATCH_SPACE(strategy)				(45 * 1024 / B40C_BFS_SG_SM20_OCCUPANCY()) 
-#define B40C_BFS_SG_SM12_SCRATCH_SPACE(strategy)				(15 * 1024 / B40C_BFS_SG_SM12_OCCUPANCY())
-#define B40C_BFS_SG_SM10_SCRATCH_SPACE(strategy)				(7  * 1024 / B40C_BFS_SG_SM10_OCCUPANCY())
-#define B40C_BFS_SG_SCRATCH_SPACE(sm_version, strategy)			((sm_version >= 200) ? B40C_BFS_SG_SM20_SCRATCH_SPACE(strategy) : 	\
-																 (sm_version >= 120) ? B40C_BFS_SG_SM12_SCRATCH_SPACE(strategy) : 	\
-																					   B40C_BFS_SG_SM10_SCRATCH_SPACE(strategy))		
+#define B40C_BFS_LEVEL_SM20_SCRATCH_SPACE(strategy)					(45 * 1024 / B40C_BFS_LEVEL_SM20_OCCUPANCY()) 
+#define B40C_BFS_LEVEL_SM12_SCRATCH_SPACE(strategy)					(15 * 1024 / B40C_BFS_LEVEL_SM12_OCCUPANCY())
+#define B40C_BFS_LEVEL_SM10_SCRATCH_SPACE(strategy)					(7  * 1024 / B40C_BFS_LEVEL_SM10_OCCUPANCY())
+#define B40C_BFS_LEVEL_SCRATCH_SPACE(sm_version, strategy)			((sm_version >= 200) ? B40C_BFS_LEVEL_SM20_SCRATCH_SPACE(strategy) : 	\
+																	 (sm_version >= 120) ? B40C_BFS_LEVEL_SM12_SCRATCH_SPACE(strategy) : 	\
+																					   B40C_BFS_LEVEL_SM10_SCRATCH_SPACE(strategy))		
 
 // Number of elements per tile.  Params: SM sm_version, strategy
-#define B40C_BFS_SG_LOG_TILE_ELEMENTS(sm_version, strategy)		(B40C_BFS_SG_LOG_CTA_THREADS(sm_version, strategy) + B40C_BFS_SG_LOG_LOAD_VEC_SIZE(sm_version, strategy))
+#define B40C_BFS_LEVEL_LOG_TILE_ELEMENTS(sm_version, strategy)		(B40C_BFS_LEVEL_LOG_CTA_THREADS(sm_version, strategy) + B40C_BFS_LEVEL_LOG_LOAD_VEC_SIZE(sm_version, strategy))
 
 // Number of elements per subtile.  Params: strategy  
-#define B40C_BFS_SG_LOG_SUBTILE_ELEMENTS(strategy)				((strategy == EXPAND_CONTRACT) ? 5 : 6)		// 64 for CONTRACT_EXPAND, 32 for EXPAND_CONTRACT
+#define B40C_BFS_LEVEL_LOG_SUBTILE_ELEMENTS(strategy)				((strategy == EXPAND_CONTRACT) ? 5 : 6)		// 64 for CONTRACT_EXPAND, 32 for EXPAND_CONTRACT
 
 
 /******************************************************************************
@@ -94,7 +95,7 @@ namespace b40c {
 
 /**
  * 
- * A single-grid breadth-first-search kernel.  (BFS-SG)
+ * A single-grid breadth-first-search kernel.  (BFS-LEVEL)
  * 
  * Marks each node with its distance from the given "source" node.  (I.e., 
  * nodes are marked with the iteration at which they were "discovered").
@@ -116,33 +117,31 @@ namespace b40c {
  */
 template <
 	typename IndexType, 
-	int STRATEGY,			// Should be of type "BfsStrategy": NVBUGS 768132
-	bool INSTRUMENTED> 						 
+	int STRATEGY>			// Should be of type "BfsStrategy": NVBUGS 768132
 __launch_bounds__ (
-	1 << B40C_BFS_SG_LOG_CTA_THREADS(__CUDA_ARCH__, STRATEGY), 
-	B40C_BFS_SG_OCCUPANCY(__CUDA_ARCH__))
-__global__ void BfsSingleGridKernel(
+	1 << B40C_BFS_LEVEL_LOG_CTA_THREADS(__CUDA_ARCH__, STRATEGY), 
+	B40C_BFS_LEVEL_OCCUPANCY(__CUDA_ARCH__))
+__global__ void BfsLevelGridKernel(
 	IndexType src,										// Source node for the first iteration
 	IndexType *d_in_queue,								// Queue of node-IDs to consume
 	IndexType *d_out_queue,								// Queue of node-IDs to produce
 	IndexType *d_column_indices,						// CSR column indices
 	IndexType *d_row_offsets,							// CSR row offsets 
 	IndexType *d_source_dist,							// Distance from the source node (initialized to -1) (per-node)
-	int *d_queue_lengths,								// Rotating 4-element array of atomic counters indicating sizes of the incoming and outgoing frontier queues
-	int *d_sync,										// Array of global synchronization counters, one for each threadblock
-	unsigned long long *d_barrier_time)					// Time (in clocks) spent by each threadblock in software global barrier
+	int *d_queue_lengths, 								// Rotating 4-element array of atomic counters indicating sizes of the incoming and outgoing frontier queues
+	IndexType iteration) 								// Current BFS iteration
 {
-	const int LOG_CTA_THREADS			= B40C_BFS_SG_LOG_CTA_THREADS(__CUDA_ARCH__, STRATEGY);
+	const int LOG_CTA_THREADS			= B40C_BFS_LEVEL_LOG_CTA_THREADS(__CUDA_ARCH__, STRATEGY);
 	const int CTA_THREADS				= 1 << LOG_CTA_THREADS;
 	
-	const int TILE_ELEMENTS 			= 1 << B40C_BFS_SG_LOG_TILE_ELEMENTS(__CUDA_ARCH__, STRATEGY);
+	const int TILE_ELEMENTS 			= 1 << B40C_BFS_LEVEL_LOG_TILE_ELEMENTS(__CUDA_ARCH__, STRATEGY);
 
-	const int LOG_SUBTILE_ELEMENTS		= B40C_BFS_SG_LOG_SUBTILE_ELEMENTS(STRATEGY);
+	const int LOG_SUBTILE_ELEMENTS		= B40C_BFS_LEVEL_LOG_SUBTILE_ELEMENTS(STRATEGY);
 	const int SUBTILE_ELEMENTS			= 1 << LOG_SUBTILE_ELEMENTS;		
 	
-	const int SCRATCH_SPACE				= B40C_BFS_SG_SCRATCH_SPACE(__CUDA_ARCH__, STRATEGY) / sizeof(IndexType);
-	const int LOAD_VEC_SIZE				= 1 << B40C_BFS_SG_LOG_LOAD_VEC_SIZE(__CUDA_ARCH__, STRATEGY);
-	const int RAKING_THREADS			= 1 << B40C_BFS_SG_LOG_RAKING_THREADS(__CUDA_ARCH__, STRATEGY);
+	const int SCRATCH_SPACE				= B40C_BFS_LEVEL_SCRATCH_SPACE(__CUDA_ARCH__, STRATEGY) / sizeof(IndexType);
+	const int LOAD_VEC_SIZE				= 1 << B40C_BFS_LEVEL_LOG_LOAD_VEC_SIZE(__CUDA_ARCH__, STRATEGY);
+	const int RAKING_THREADS			= 1 << B40C_BFS_LEVEL_LOG_RAKING_THREADS(__CUDA_ARCH__, STRATEGY);
 	
 	// Number of scan partials for a tile
 	const int LOG_SCAN_PARTIALS 		= LOG_CTA_THREADS;			// One partial per thread
@@ -172,7 +171,7 @@ __global__ void BfsSingleGridKernel(
 	const CacheModifier QUEUE_MODIFIER						= CG;
 	const CacheModifier COLUMN_INDICES_MODIFIER				= CG;
 	const CacheModifier SOURCE_DIST_MODIFIER				= CG;
-	const CacheModifier ROW_OFFSETS_MODIFIER				= CG;
+	const CacheModifier ROW_OFFSETS_MODIFIER				= CA;
 	const CacheModifier MISALIGNED_ROW_OFFSETS_MODIFIER		= CA;
 	
 	SuppressUnusedConstantWarning(CTA_THREADS);
@@ -192,14 +191,10 @@ __global__ void BfsSingleGridKernel(
 	int* 		scan_pool = reinterpret_cast<int*>(aligned_smem_pool);				// The smem pool for (i) above
 	IndexType* 	scratch_pool = reinterpret_cast<IndexType*>(aligned_smem_pool);		// The smem pool for (ii) above
 	
-	__shared__ int s_num_incoming_nodes;	// Number of elements in the incoming frontier queue for the current iteration
 	__shared__ int s_cta_offset;			// Offset in the incoming frontier queue for this CTA to begin raking its tiles
 	__shared__ int s_cta_extra_elements;	// Number of elements in a last, partially-full tile (needing guarded loads)   
 	__shared__ int s_cta_out_of_bounds;		// The offset in the incoming frontier for this CTA to stop raking full tiles
 
-	unsigned long long barrier_time = 0;
-	unsigned int total_queued;
-	IndexType iteration = 0;				// Current BFS iteration
 
 	
 	//
@@ -225,98 +220,39 @@ __global__ void BfsSingleGridKernel(
 	int col = threadIdx.x & (PARTIALS_PER_ROW - 1); 
 	int *base_partial = scan_pool + (row * PADDED_PARTIALS_PER_ROW) + col;
 	
-	// First iteration: process the source node specified in the formal parameters
 	if (threadIdx.x == 0) {
 	
-		total_queued = (STRATEGY == EXPAND_CONTRACT) ? 1 : 0;
-		s_num_incoming_nodes = 1;
-		s_cta_offset = 0;
-		s_cta_out_of_bounds = 0;
-		
-		if (blockIdx.x == 0) {
-			
-			// First CTA resets global atomic counter for future outgoing counter
-			int future_queue_length_idx = (iteration + 2) & 0x3; 	// Index of the future outgoing queue length (must reset for the next iteration) 
-			d_queue_lengths[future_queue_length_idx] = 0;
+		if (iteration == 0) { 
 
-			// Only the first CTA does any work (so enqueue it, and reset outgoing queue length) 
-			s_cta_extra_elements = 1;
-			d_in_queue[0] = src;
-			d_queue_lengths[1] = 0;
+			// First iteration: process the source node specified in the formal parameters
+			s_cta_offset = 0;
+			s_cta_out_of_bounds = 0;
 			
-			// Expand-contract algorithm requires setting source to already-discovered
-			if (STRATEGY == EXPAND_CONTRACT) {
-				d_source_dist[src] = 0;
-			}
-		} else {
-
-			// No work for all other CTAs
-			s_cta_extra_elements = 0;
-		}
-	}
+			if (blockIdx.x == 0) {
+				
+				// First CTA resets global atomic counter for future outgoing counter
+				int future_queue_length_idx = (iteration + 2) & 0x3; 	// Index of the future outgoing queue length (must reset for the next iteration) 
+				d_queue_lengths[future_queue_length_idx] = 0;
 	
-	__syncthreads();
-
-	//
-	// Loop over BFS frontier queue iterations
-	//
-	do {
-
-		// Index of the outgoing queue length
-		int outgoing_queue_length_idx = (iteration + 1) & 0x3; 				
-
-		// Perform a pass through the incoming frontier queue
-		if (iteration & 0x1) {
-		
-			// Odd iteration: frontier queue streamed out -> in 
-			BfsIteration<(BfsStrategy) STRATEGY, IndexType, CTA_THREADS, TILE_ELEMENTS, PARTIALS_PER_SEG, SCRATCH_SPACE, 
-					LOAD_VEC_SIZE, QUEUE_MODIFIER, COLUMN_INDICES_MODIFIER, SOURCE_DIST_MODIFIER,
-					ROW_OFFSETS_MODIFIER, MISALIGNED_ROW_OFFSETS_MODIFIER>(
-				iteration, scratch_pool, base_partial, raking_segment, warpscan, 
-				d_out_queue, d_in_queue, d_column_indices, d_row_offsets, d_source_dist, d_queue_lengths + outgoing_queue_length_idx,
-				s_enqueue_offset, s_cta_offset, s_cta_extra_elements, s_cta_out_of_bounds);
-
+				// Only the first CTA does any work (so enqueue it, and reset outgoing queue length) 
+				s_cta_extra_elements = 1;
+				d_in_queue[0] = src;
+				d_queue_lengths[1] = 0;
+				
+				// Expand-contract algorithm requires setting source to already-discovered
+				if (STRATEGY == EXPAND_CONTRACT) {
+					d_source_dist[src] = 0;
+				}
+				
+			} else {
+	
+				// No work for all other CTAs
+				s_cta_extra_elements = 0;
+			}
+			
 		} else {
 			
-			// Even iteration: frontier queue streamed in -> out  
-			BfsIteration<(BfsStrategy) STRATEGY, IndexType, CTA_THREADS, TILE_ELEMENTS, PARTIALS_PER_SEG, SCRATCH_SPACE, 
-					LOAD_VEC_SIZE, QUEUE_MODIFIER, COLUMN_INDICES_MODIFIER, SOURCE_DIST_MODIFIER,
-					ROW_OFFSETS_MODIFIER, MISALIGNED_ROW_OFFSETS_MODIFIER>(
-				iteration, scratch_pool, base_partial, raking_segment, warpscan, 
-				d_in_queue, d_out_queue, d_column_indices, d_row_offsets, d_source_dist, d_queue_lengths + outgoing_queue_length_idx,
-				s_enqueue_offset, s_cta_offset, s_cta_extra_elements, s_cta_out_of_bounds);
-		}
-
-		//
-		// Global software barrier to make queues coherent between BFS iterations
-		//
-		
-		clock_t start;
-		if (INSTRUMENTED) {
-			// Instrumented timer for global barrier
-			if (threadIdx.x == 0) {
-				start = clock();
-			}
-		}
-		
-		GlobalBarrier(d_sync);
-
-		if (INSTRUMENTED) {
-			// Instrumented timer for global barrier
-			if (threadIdx.x == 0) {
-				clock_t stop = clock();
-				if (stop >= start) {
-					barrier_time += stop - start;
-				} else {
-					barrier_time += stop + (((clock_t) -1) - start);
-				}
-			}
-		}		
-		
-		iteration++;
-		
-		// Calculate our CTA's work range for the next BFS iteration
-		if (threadIdx.x == 0) {
+			// Calculate our CTA's work range for a subsequent BFS iteration
 
 			// First CTA resets global atomic counter for future outgoing counter
 			if (blockIdx.x == 0) {
@@ -328,7 +264,6 @@ __global__ void BfsSingleGridKernel(
 			int num_incoming_nodes;
 			int incoming_queue_length_idx = (iteration + 0) & 0x3;	// Index of incoming queue length
 			ModifiedLoad<int, CG>::Ld(num_incoming_nodes, d_queue_lengths, incoming_queue_length_idx);
-			total_queued += num_incoming_nodes;
 	
 			//
 			// Although work is done in "tile"-sized blocks, work is assigned 
@@ -376,33 +311,25 @@ __global__ void BfsSingleGridKernel(
 			}
 
 			// Store results for the rest of the CTA
-			s_num_incoming_nodes = num_incoming_nodes;
 			s_cta_offset = cta_offset;
 			s_cta_out_of_bounds = cta_out_of_bounds;
 			s_cta_extra_elements = cta_extra_elements;
-		}
-		
-		__syncthreads();
-	
-	} while (s_num_incoming_nodes > 0);
-	
-	// All done
-	
-	if (INSTRUMENTED) {
-		// Report statistics
-		if (threadIdx.x == 0) {
-			
-			// Stash the barrier time for our block
-			d_barrier_time[blockIdx.x] = barrier_time; 
-	
-			if (blockIdx.x == 0) {
-				
-				// Stash the total number of queued items and iterations
-				d_queue_lengths[4] = total_queued;
-				d_queue_lengths[5] = iteration;
-			}
+
 		}
 	}
+	
+	__syncthreads();
+
+	// Index of the outgoing queue length
+	int outgoing_queue_length_idx = (iteration + 1) & 0x3; 				
+
+	// Perform a pass through the incoming frontier queue
+	BfsIteration<(BfsStrategy) STRATEGY, IndexType, CTA_THREADS, TILE_ELEMENTS, PARTIALS_PER_SEG, SCRATCH_SPACE, 
+			LOAD_VEC_SIZE, QUEUE_MODIFIER, COLUMN_INDICES_MODIFIER, SOURCE_DIST_MODIFIER,
+			ROW_OFFSETS_MODIFIER, MISALIGNED_ROW_OFFSETS_MODIFIER>(
+		iteration, scratch_pool, base_partial, raking_segment, warpscan, 
+		d_in_queue, d_out_queue, d_column_indices, d_row_offsets, d_source_dist, d_queue_lengths + outgoing_queue_length_idx,
+		s_enqueue_offset, s_cta_offset, s_cta_extra_elements, s_cta_out_of_bounds);
 } 
 
 
