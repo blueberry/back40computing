98bb3381a06d83d9116b4aa8786c12497b28228e
duane.merrill <duane.merrill@f28d7702-48ea-1802-4fdf-2413024927d0>
2011-02-17 05:19:38 +0000

    Adding Memcopy project

---
+++ Memcopy/examples/test_memcopy_large.cu
@@ -0,0 +1,291 @@
+/******************************************************************************
+ * 
+ * Copyright 2010-2011 Duane Merrill
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ * 
+ * For more information, see our Google Code project site: 
+ * http://code.google.com/p/back40computing/
+ * 
+ ******************************************************************************/
+
+
+/******************************************************************************
+ * Simple test driver program for *large-problem* memcopy.
+ ******************************************************************************/
+
+#include <stdio.h> 
+
+// Memcopy includes
+#include "memcopy_api_granularity.cuh"
+#include "memcopy_api_enactor.cuh"
+
+// Test utils
+#include "b40c_util.h"					// Misc. utils (random-number gen, I/O, etc.)
+
+using namespace b40c;
+using namespace memcopy;
+
+// #define DEBUG
+
+
+/******************************************************************************
+ * Defines, constants, globals 
+ ******************************************************************************/
+
+bool g_verbose;
+int g_max_ctas = 0;
+
+
+/******************************************************************************
+ * Test structures
+ ******************************************************************************/
+
+// Test value-type structure 
+struct Fribbitz {
+	char a;
+	double b;
+	unsigned short c;
+};
+
+
+/******************************************************************************
+ * Routines
+ ******************************************************************************/
+
+/**
+ * Displays the commandline usage for this tool
+ */
+void Usage() 
+{
+	printf("\ntest_memcopy_large [--device=<device index>] [--v] [--i=<num-iterations>] "
+			"[--max-ctas=<max-thread-blocks>] [--n=<num-elements>]\n");
+	printf("\n");
+	printf("\t--v\tDisplays copied results to the console.\n");
+	printf("\n");
+	printf("\t--i\tPerforms the memcopy operation <num-iterations> times\n");
+	printf("\t\t\ton the device. Re-copies original input each time. Default = 1\n");
+	printf("\n");
+	printf("\t--n\tThe number of elements to comprise the sample problem\n");
+	printf("\t\t\tDefault = 512\n");
+	printf("\n");
+}
+
+
+
+/**
+ * Timed memcopy.  Uses the GPU to copy the specified vector of elements for the given
+ * number of iterations, displaying runtime information.
+ *
+ * @param[in] 		num_elements 
+ * 		Size in elements of the vector to copy
+ * @param[in] 		h_data
+ * 		Vector of data to copy (also copied back out)
+ * @param[in] 		iterations  
+ * 		Number of times to invoke the GPU memcopy primitive
+ */
+template <typename T, typename IndexType>
+void TimedMemcopy(
+	IndexType num_elements,
+	T *h_data,
+	int iterations)
+{
+	printf("%d iterations, %d elements", iterations, num_elements);
+	
+	// Allocate device storage  
+	T *d_src, *d_dest;
+	if (B40CPerror(cudaMalloc((void**) &d_src, sizeof(T) * num_elements),
+		"TimedMemcopy cudaMalloc d_src failed: ", __FILE__, __LINE__)) exit(1);
+	if (B40CPerror(cudaMalloc((void**) &d_dest, sizeof(T) * num_elements),
+		"TimedMemcopy cudaMalloc d_dest failed: ", __FILE__, __LINE__)) exit(1);
+
+	// Create memcopy enactor
+	MemcopyEnactor<> memcopy_enactor;
+
+	// Establish granularity configuration type
+	typedef MemcopyConfig<
+		T,
+		IndexType,
+		9,									// 512 items
+		8,									// 8 CTAs/SM
+		7,									// 128 threads
+		1,									// vec-2
+		1,									// 2 loads per tile
+		NONE> Config;
+
+	// Perform a single memcopy iteration to allocate any memory if needed, prime code caches, etc.
+	if (B40CPerror(cudaMemcpy(d_src, h_data, sizeof(T) * num_elements, cudaMemcpyHostToDevice),
+		"TimedMemcopy cudaMemcpy d_src failed: ", __FILE__, __LINE__)) exit(1);
+	memcopy_enactor.DEBUG = true;
+	memcopy_enactor.Enact<Config>(d_dest, d_src, num_elements, g_max_ctas);
+	memcopy_enactor.DEBUG = false;
+
+	// Perform the timed number of memcopy iterations
+
+	cudaEvent_t start_event, stop_event;
+	cudaEventCreate(&start_event);
+	cudaEventCreate(&stop_event);
+
+	double elapsed = 0;
+	float duration = 0;
+	for (int i = 0; i < iterations; i++) {
+
+		// Move a fresh copy of the problem into device storage
+		if (B40CPerror(cudaMemcpy(d_src, h_data, sizeof(T) * num_elements, cudaMemcpyHostToDevice),
+			"TimedMemcopy cudaMemcpy d_src failed: ", __FILE__, __LINE__)) exit(1);
+
+		// Start cuda timing record
+		cudaEventRecord(start_event, 0);
+
+		// Call the memcopy API routine
+		memcopy_enactor.Enact<Config>(d_dest, d_src, num_elements, g_max_ctas);
+
+		// End cuda timing record
+		cudaEventRecord(stop_event, 0);
+		cudaEventSynchronize(stop_event);
+		cudaEventElapsedTime(&duration, start_event, stop_event);
+		elapsed += (double) duration;		
+	}
+
+	// Display timing information
+	double avg_runtime = elapsed / iterations;
+	double throughput = ((double) num_elements) / avg_runtime / 1000.0 / 1000.0; 
+    printf(", %f GPU ms, %f x10^9 elts/sec, %f x10^9 B/sec\n",
+		avg_runtime, throughput, 2 * throughput * sizeof(T));
+	
+    // Clean up events
+	cudaEventDestroy(start_event);
+	cudaEventDestroy(stop_event);
+
+    // Copy out data
+    if (B40CPerror(cudaMemcpy(h_data, d_dest, sizeof(T) * num_elements, cudaMemcpyDeviceToHost),
+		"TimedMemcopy cudaMemcpy d_dest failed: ", __FILE__, __LINE__)) exit(1);
+    
+    // Free allocated memory
+    if (d_src) cudaFree(d_src);
+    if (d_dest) cudaFree(d_dest);
+}
+
+
+/**
+ * Creates an example memcopy problem and then dispatches the problem
+ * to the GPU for the given number of iterations, displaying runtime information.
+ *
+ * @param[in] 		iterations  
+ * 		Number of times to invoke the GPU memcopy primitive
+ * @param[in] 		num_elements 
+ * 		Size in elements of the vector to copy
+ */
+template<typename T, typename IndexType>
+void TestMemcopy(
+	int iterations,
+	IndexType num_elements)
+{
+    // Allocate the memcopy problem on the host and fill the keys with random bytes
+
+	T *h_data 			= (T*) malloc(num_elements * sizeof(T));
+	T *h_reference 		= (T*) malloc(num_elements * sizeof(T));
+
+	for (IndexType i = 0; i < num_elements; ++i) {
+//		RandomBits<T>(h_data[i], 0);
+		h_data[i] = i;
+		h_reference[i] = h_data[i];
+	}
+
+    // Run the timing test
+	TimedMemcopy<T, IndexType>(num_elements, h_data, iterations);
+
+	// Flushes any stdio from the GPU
+	cudaThreadSynchronize();
+    
+	// Display copied data
+	if (g_verbose) {
+		printf("\n\nData:\n");
+		for (int i = 0; i < num_elements; i++) {	
+			PrintValue<T>(h_data[i]);
+			printf(", ");
+		}
+		printf("\n\n");
+	}	
+	
+    // Verify solution
+	CompareResults<T>(h_data, h_reference, num_elements, true);
+	printf("\n");
+	fflush(stdout);
+
+	// Free our allocated host memory 
+	if (h_data) free(h_data);
+    if (h_reference) free(h_reference);
+}
+
+
+/******************************************************************************
+ * Main
+ ******************************************************************************/
+
+int main(int argc, char** argv)
+{
+
+	CommandLineArgs args(argc, argv);
+	DeviceInit(args);
+
+	//srand(time(NULL));	
+	srand(0);				// presently deterministic
+
+    int num_elements 					= 1024;
+    int iterations  					= 1;
+
+    //
+	// Check command line arguments
+    //
+
+    if (args.CheckCmdLineFlag("help")) {
+		Usage();
+		return 0;
+	}
+
+    args.GetCmdLineArgumenti("i", iterations);
+    args.GetCmdLineArgumenti("n", num_elements);
+    args.GetCmdLineArgumenti("max-ctas", g_max_ctas);
+	g_verbose = args.CheckCmdLineFlag("v");
+
+	typedef int IndexType;
+
+/*	
+	// Execute test(s)
+	TestMemcopy<unsigned char, IndexType>(
+			iterations,
+			num_elements);
+	TestMemcopy<unsigned short, IndexType>(
+			iterations,
+			num_elements);
+	TestMemcopy<unsigned int, IndexType>(
+			iterations,
+			num_elements);
+	TestMemcopy<unsigned long long, IndexType>(
+			iterations,
+			num_elements);
+	TestMemcopy<Fribbitz, IndexType>(
+			iterations,
+			num_elements);
+*/
+
+	TestMemcopy<unsigned int, IndexType>(
+			iterations,
+			num_elements);
+
+	return 0;
+}
+
+
+
---
+++ Memcopy/memcopy_api_enactor.cuh
@@ -0,0 +1,246 @@
+/******************************************************************************
+ * 
+ * Copyright 2010-2011 Duane Merrill
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ * 
+ * For more information, see our Google Code project site: 
+ * http://code.google.com/p/back40computing/
+ * 
+ ******************************************************************************/
+
+/******************************************************************************
+ * Base Memcopy Enactor
+ ******************************************************************************/
+
+#pragma once
+
+#include <stdio.h>
+#include "b40c_kernel_utils.cuh"
+#include "memcopy_kernel.cuh"
+
+namespace b40c {
+using namespace memcopy;
+
+
+/**
+ * Basic mem-copy enactor class.
+ */
+template <typename DerivedEnactorType = void>
+class MemcopyEnactor
+{
+protected:
+
+	//---------------------------------------------------------------------
+	// Specialize templated dispatch to self (no derived class) or 
+	// derived class (CRTP -- curiously recurring template pattern)
+	//---------------------------------------------------------------------
+
+	template <typename DerivedType, int __dummy = 0>
+	struct DispatchType 
+	{
+		typedef DerivedType Type;
+	};
+	
+	template <int __dummy>
+	struct DispatchType<void, __dummy>
+	{
+		typedef MemcopyEnactor<void> Type;
+	};
+
+	
+	//---------------------------------------------------------------------
+	// Members
+	//---------------------------------------------------------------------
+		
+	// Device properties
+	const CudaProperties cuda_props;
+
+
+	//-----------------------------------------------------------------------------
+	// Utility Routines
+	//-----------------------------------------------------------------------------
+	
+	/**
+	 * Returns the number of threadblocks that the specified device should 
+	 * launch for [up|down]sweep grids for the given problem size
+	 */
+	template <typename MemcopyConfig>
+	int SweepGridSize(int num_elements, int max_grid_size) 
+	{
+		const int SCHEDULE_GRANULARITY = 1 << MemcopyConfig::LOG_SCHEDULE_GRANULARITY;
+
+		int default_sweep_grid_size;
+		if (cuda_props.device_sm_version < 120) {
+			
+			// G80/G90: Four times the SM-count
+			default_sweep_grid_size = cuda_props.device_props.multiProcessorCount * 4;
+			
+		} else if (cuda_props.device_sm_version < 200) {
+			
+			// GT200: Special sauce
+			
+			// Start with with full downsweep occupancy of all SMs 
+			default_sweep_grid_size = 
+				cuda_props.device_props.multiProcessorCount * MemcopyConfig::CTA_OCCUPANCY;
+
+			// Increase by default every 64 million key-values
+			int step = 1024 * 1024 * 64;		 
+			default_sweep_grid_size *= (num_elements + step - 1) / step;
+
+			double multiplier1 = 4.0;
+			double multiplier2 = 16.0;
+
+			double delta1 = 0.068;
+			double delta2 = 0.1285;
+			
+			int dividend = (num_elements + 512 - 1) / 512;
+
+			int bumps = 0;
+			while(true) {
+
+				if (default_sweep_grid_size <= cuda_props.device_props.multiProcessorCount) {
+					break;
+				}
+				
+				double quotient = ((double) dividend) / (multiplier1 * default_sweep_grid_size);
+				quotient -= (int) quotient;
+
+				if ((quotient > delta1) && (quotient < 1 - delta1)) {
+
+					quotient = ((double) dividend) / (multiplier2 * default_sweep_grid_size / 3.0);
+					quotient -= (int) quotient;
+
+					if ((quotient > delta2) && (quotient < 1 - delta2)) {
+						break;
+					}
+				}
+
+				if (bumps == 3) {
+					// Bump it down by 27
+					default_sweep_grid_size -= 27;
+					bumps = 0;
+				} else {
+					// Bump it down by 1
+					default_sweep_grid_size--;
+					bumps++;
+				}
+			}
+			
+		} else {
+
+			// GF10x
+			if (cuda_props.device_sm_version == 210) {
+				// GF110
+				default_sweep_grid_size = 4 * (cuda_props.device_props.multiProcessorCount * MemcopyConfig::CTA_OCCUPANCY);
+			} else {
+				// Anything but GF110
+				default_sweep_grid_size = 4 * (cuda_props.device_props.multiProcessorCount * MemcopyConfig::CTA_OCCUPANCY) - 2;
+			}
+		}
+		
+		// Reduce by override, if specified
+		if (max_grid_size > 0) {
+			default_sweep_grid_size = max_grid_size;
+		}
+		
+		// Reduce if we have less work than we can divide up among this 
+		// many CTAs
+		
+		int grains = (num_elements + SCHEDULE_GRANULARITY - 1) / SCHEDULE_GRANULARITY;
+		if (default_sweep_grid_size > grains) {
+			default_sweep_grid_size = grains;
+		}
+		
+		return default_sweep_grid_size;
+	}	
+
+
+public:
+
+	/**
+	 * Debug level.  If set, the enactor blocks after kernel calls to check
+	 * for successful launch/execution
+	 */
+	bool DEBUG;
+
+
+	/**
+	 * Constructor.
+	 */
+	MemcopyEnactor() :
+#if	defined(__THRUST_SYNCHRONOUS) || defined(DEBUG) || defined(_DEBUG)
+			DEBUG(true)
+#else
+			DEBUG(false)
+#endif
+		{}
+
+    
+	/**
+	 * Enacts a memcopy on the specified device data.
+	 *
+	 * For generating memcopy kernels having computational granularities in accordance
+	 * with user-supplied granularity-specialization types.  (Useful for auto-tuning.)
+	 *
+	 *
+	 * @return cudaSuccess on success, error enumeration otherwise
+	 */
+	template <typename MemcopyConfig>
+	cudaError_t Enact(
+		typename MemcopyConfig::T *d_dest,
+		typename MemcopyConfig::T *d_src,
+		typename MemcopyConfig::IndexType length,
+		int max_grid_size = 0)
+	{
+		typedef typename MemcopyConfig::T T;
+		typedef typename MemcopyConfig::IndexType IndexType;
+		typedef MemcopyKernelConfig<MemcopyConfig> MemcopyKernelConfigType;
+
+		const int SCHEDULE_GRANULARITY 	= 1 << MemcopyConfig::LOG_SCHEDULE_GRANULARITY;
+
+		int sweep_grid_size = SweepGridSize<MemcopyConfig>(length, max_grid_size);
+		int dynamic_smem = 0;
+		
+		CtaWorkDistribution<IndexType> work(length, SCHEDULE_GRANULARITY, sweep_grid_size);
+
+		if (DEBUG) {
+			printf("\n\n");
+			printf("CodeGen: \t[device_sm_version: %d, kernel_ptx_version: %d]\n", 
+				cuda_props.device_sm_version, cuda_props.kernel_ptx_version);
+			printf("Memcopy: \t[grid_size: %d, threads %d]\n",
+				sweep_grid_size, 1 << MemcopyConfig::LOG_THREADS);
+			printf("Work: \t\t[num_elements: %d, schedule_granularity: %d, total_grains: %d, grains_per_cta: %d, extra_grains: %d]\n",
+				work.num_elements, SCHEDULE_GRANULARITY, work.total_grains, work.grains_per_cta, work.extra_grains);
+			printf("\n\n");
+		}
+		
+		cudaError_t retval = cudaSuccess;
+		do {
+
+			// Invoke memcopy kernel
+			MemcopyKernel<MemcopyKernelConfigType><<<sweep_grid_size, MemcopyKernelConfigType::THREADS, dynamic_smem>>>(
+				d_dest, d_src, work);
+			if (DEBUG && (retval = B40CPerror(cudaThreadSynchronize(),
+				"MemcopyEnactor:: MemcopyKernel failed ", __FILE__, __LINE__))) break;
+
+		} while (0);
+
+	    return retval;
+	}
+};
+
+
+
+}// namespace b40c
+
