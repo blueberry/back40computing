6decba003957e667398f882a8b5fff1c2e948e1c
duane.merrill <duane.merrill@f28d7702-48ea-1802-4fdf-2413024927d0>
2011-07-06 03:33:03 +0000

    updated bfs test scripts

--- trunk/test/bfs/run_random_tests.sh
+++ trunk/test/bfs/run_random_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-OPTIONS="--i=100 --src=randomize --num-gpus=4 --queue-sizing=0.9"
+OPTIONS="--i=100 --src=randomize --num-gpus=4 --queue-sizing=0.95 --quick"
 
 for j in 2 4 8 16
 do 
@@ -14,7 +14,7 @@ do
 			echo ./bin/test_bfs_4.0_x86_64 dimacs ../../../graphs/random/random.${j}Mv.$((j*8))Me.gr --splice=$i $OPTIONS ... eval/random.4x.$((i*8))EF.${j}Mv.$((i*j*8))Me.txt
 			./bin/test_bfs_4.0_x86_64 dimacs ../../../graphs/random/random.${j}Mv.$((j*8))Me.gr --splice=$i $OPTIONS > eval/random.4x.$((i*8))EF.${j}Mv.$((i*j*8))Me.txt
 
-			echo ./bin/test_bfs_4.0_x86_64 dimacs ../../../graphs/random/random.${j}Mv.$((j*8))Me.gr --splice=$i --mark-parents $OPTIONS ... eval/random.4x.$((i*8))EF.${j}Mv.$((i*j*8))Me.txt
+			echo ./bin/test_bfs_4.0_x86_64 dimacs ../../../graphs/random/random.${j}Mv.$((j*8))Me.gr --splice=$i --mark-parents $OPTIONS ... eval/random.4x.$((i*8))EF.${j}Mv.$((i*j*8))Me.parents.txt
 			./bin/test_bfs_4.0_x86_64 dimacs ../../../graphs/random/random.${j}Mv.$((j*8))Me.gr --splice=$i --mark-parents $OPTIONS > eval/random.4x.$((i*8))EF.${j}Mv.$((i*j*8))Me.parents.txt
 
 		fi
--- trunk/test/bfs/run_tests.sh
+++ trunk/test/bfs/run_tests.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+#./run_random_tests.sh
 ./run_tests_4gpu.sh
 ./run_tests_3gpu.sh
 ./run_tests_2gpu.sh
