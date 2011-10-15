b7f556857cc6e36b1c29b2e5f730386c8580427d
duane.merrill <duane.merrill@f28d7702-48ea-1802-4fdf-2413024927d0>
2011-10-15 20:48:32 +0000

    Updates to scan autotuning

---
+++ trunk/test/scan/run_tune_tests.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#for i in 200 130 110; do for j in 1 2 4 8; do time make tune tunearch=$i tunesize=$j force64=1; done; done
+
+OPTIONS="--i=2 --device=1"
+
+if [ $# -eq 0 ]
+then
+	echo "Usage: $0 <ARCH>"
+	exit 1
+fi
+
+ARCH=$1
+
+mkdir -p eval/sm${ARCH}
+
+
+for WORD in 1 2 4 8
+do
+	# 32K..32M
+	for (( N=1024*32; N<=1024*1024*32; N*=4 ))
+	do
+
+		CMD="./bin/tune_scan_4.1_i386_sm${ARCH}_u${WORD}B ${OPTIONS} --n=${N}"
+		echo "$CMD > eval/sm${ARCH}/${ARCH}_${WORD}B_${N}.txt"
+		$CMD > eval/sm${ARCH}/${ARCH}_${WORD}B_${N}.txt
+
+	done 
+done
