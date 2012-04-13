f651b38b39cc38a082d56048b18cde61b1e842d0
duane.merrill <duane.merrill@f28d7702-48ea-1802-4fdf-2413024927d0>
2012-04-13 05:42:21 +0000

    Update to GF100 grid size heuristics Update to -open64 flag in makefile

--- branches/FastSortSm20/b40c/util/enactor_base.cuh
+++ branches/FastSortSm20/b40c/util/enactor_base.cuh
@@ -342,12 +342,15 @@ protected:
 
 		} else {
 
+			int saturation_cap  = (4 * max_cta_occupancy * cuda_props.device_props.multiProcessorCount) - 1;
+
 			// GF10x
 			grid_size = B40C_MIN(
 				grains,
 				((max_grid_size > 0) ?
 					max_grid_size :
-					(max_cta_occupancy * max_cta_occupancy * cuda_props.device_props.multiProcessorCount) - 1));
+					saturation_cap));
+
 		}
 
 		return grid_size;
