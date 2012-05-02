4b8e389e10442e5af1b4e45c2f14baefd2027f71
duane.merrill <duane.merrill@f28d7702-48ea-1802-4fdf-2413024927d0>
2012-05-02 00:32:34 +0000

    sort refactor part 1

--- b40c/radix_sort/upsweep/cta.cuh
+++ b40c/radix_sort/upsweep/cta.cuh
@@ -39,22 +39,76 @@ namespace upsweep {
  * Derives from partition::upsweep::Cta
  */
 template <typename KernelPolicy>
-struct Cta :
-	partition::upsweep::Cta<
-		KernelPolicy,
-		Cta<KernelPolicy>,			// This class
-		Tile>						// radix_sort::upsweep::Tile
+struct Cta
 {
 	//---------------------------------------------------------------------
 	// Typedefs and Constants
 	//---------------------------------------------------------------------
 
-	// Base class type
-	typedef partition::upsweep::Cta<KernelPolicy, Cta, Tile> Base;
-
 	typedef typename KernelPolicy::KeyType 					KeyType;
 	typedef typename KernelPolicy::SizeT 					SizeT;
 	typedef typename KernelPolicy::SmemStorage				SmemStorage;
+
+
+	//---------------------------------------------------------------------
+	// Members
+	//---------------------------------------------------------------------
+
+	// Shared storage for this CTA
+	typename KernelPolicy::SmemStorage 	&smem_storage;
+
+	// Shared-memory lanes of composite-counters
+	CompostiteCounters<KernelPolicy> 	composite_counters;
+
+	// Thread-local counters for periodically aggregating composite-counter lanes
+	AggregateCounters<KernelPolicy>		aggregate_counters;
+
+	// Input and output device pointers
+	KeyType								*d_in_keys;
+	SizeT								*d_spine;
+
+	int 								warp_id;
+	int 								warp_idx;
+
+	char 								*base;
+
+
+	//---------------------------------------------------------------------
+	// Helper Structures
+	//---------------------------------------------------------------------
+
+	/**
+	 * Unrolled tile processing
+	 */
+	struct UnrollTiles
+	{
+		// Recurse over counts
+		template <int UNROLL_COUNT, int __dummy = 0>
+		struct Iterate
+		{
+			static const int HALF = UNROLL_COUNT / 2;
+
+			template <typename Cta>
+			static __device__ __forceinline__ void ProcessTiles(
+				Cta *cta, SizeT cta_offset)
+			{
+				Iterate<HALF>::ProcessTiles(cta, cta_offset);
+				Iterate<HALF>::ProcessTiles(cta, cta_offset + (KernelPolicy::TILE_ELEMENTS * HALF));
+			}
+		};
+
+		// Terminate (process one tile)
+		template <int __dummy>
+		struct Iterate<1, __dummy>
+		{
+			template <typename Cta>
+			static __device__ __forceinline__ void ProcessTiles(
+				Cta *cta, SizeT cta_offset)
+			{
+				cta->ProcessFullTile(cta_offset);
+			}
+		};
+	};
 
 
 	//---------------------------------------------------------------------
@@ -68,9 +122,161 @@ struct Cta :
 		SmemStorage 	&smem_storage,
 		KeyType 		*d_in_keys,
 		SizeT 			*d_spine) :
-			Base(smem_storage, d_in_keys, d_spine)
-	{}
+			smem_storage(smem_storage),
+			d_in_keys(d_in_keys),
+			d_spine(d_spine),
+			warp_id(threadIdx.x >> B40C_LOG_WARP_THREADS(__B40C_CUDA_ARCH__)),
+			warp_idx(util::LaneId())
+	{
+		base = (char *) (smem_storage.composite_counters.words[warp_id] + warp_idx);
+	}
 
+
+	/**
+	 * Processes a single, full tile
+	 */
+	__device__ __forceinline__ void ProcessFullTile(
+		SizeT cta_offset)
+	{
+		Tile<
+			KernelPolicy::LOG_LOADS_PER_TILE,
+			KernelPolicy::LOG_LOAD_VEC_SIZE,
+			KernelPolicy> tile;
+
+		// Load keys
+		tile.LoadKeys(this, cta_offset);
+
+		// Prevent bucketing from being hoisted (otherwise we don't get the desired outstanding loads)
+		if (KernelPolicy::LOADS_PER_TILE > 1) __syncthreads();
+
+		// Bucket tile of keys
+		tile.Bucket(this);
+
+		// Store keys (if necessary)
+		tile.StoreKeys(this, cta_offset);
+	}
+
+
+	/**
+	 * Processes a single load (may have some threads masked off)
+	 */
+	__device__ __forceinline__ void ProcessPartialTile(
+		SizeT cta_offset,
+		const SizeT &out_of_bounds)
+	{
+		// Process partial tile if necessary using single loads
+		while (cta_offset + threadIdx.x < out_of_bounds) {
+
+			Tile<0, 0, KernelPolicy> tile;
+
+			// Load keys
+			tile.LoadKeys(this, cta_offset);
+
+			// Bucket tile of keys
+			tile.Bucket(this);
+
+			// Store keys (if necessary)
+			tile.StoreKeys(this, cta_offset);
+
+			cta_offset += KernelPolicy::THREADS;
+		}
+	}
+
+
+	/**
+	 * Process work range of tiles
+	 */
+	__device__ __forceinline__ void ProcessWorkRange(
+		util::CtaWorkLimits<SizeT> &work_limits)
+	{
+		// Make sure we get a local copy of the cta's offset (work_limits may be in smem)
+		SizeT cta_offset = work_limits.offset;
+
+		aggregate_counters.ResetCounters(this);
+		composite_counters.ResetCompositeCounters(this);
+
+
+#if 1	// Use deep unrolling for better instruction efficiency
+
+		// Unroll batches of full tiles
+		const int UNROLLED_ELEMENTS = KernelPolicy::UNROLL_COUNT * KernelPolicy::TILE_ELEMENTS;
+		while (cta_offset  + UNROLLED_ELEMENTS < work_limits.out_of_bounds) {
+
+			UnrollTiles::template Iterate<KernelPolicy::UNROLL_COUNT>::ProcessTiles(
+				this,
+				cta_offset);
+			cta_offset += UNROLLED_ELEMENTS;
+
+			__syncthreads();
+
+			// Aggregate back into local_count registers to prevent overflow
+			aggregate_counters.ExtractComposites(this);
+
+			__syncthreads();
+
+			// Reset composite counters in lanes
+			composite_counters.ResetCompositeCounters(this);
+		}
+
+		// Unroll single full tiles
+		while (cta_offset < work_limits.guarded_offset) {
+
+			ProcessFullTile(cta_offset);
+			cta_offset += KernelPolicy::TILE_ELEMENTS;
+		}
+
+#else 	// Use shallow unrolling for faster compilation tiles
+
+		// Unroll single full tiles
+		while (cta_offset < work_limits.guarded_offset) {
+
+			ProcessFullTile(cta_offset);
+			cta_offset += KernelPolicy::TILE_ELEMENTS;
+
+			const SizeT UNROLL_MASK = (KernelPolicy::UNROLL_COUNT - 1) << KernelPolicy::LOG_TILE_ELEMENTS;
+			if ((cta_offset & UNROLL_MASK) == 0) {
+
+				__syncthreads();
+
+				// Aggregate back into local_count registers to prevent overflow
+				aggregate_counters.ExtractComposites(this);
+
+				__syncthreads();
+
+				// Reset composite counters in lanes
+				composite_counters.ResetCompositeCounters(this);
+			}
+		}
+#endif
+
+		// Process partial tile if necessary
+		ProcessPartialTile(cta_offset, work_limits.out_of_bounds);
+
+		__syncthreads();
+
+		// Aggregate back into local_count registers
+		aggregate_counters.ExtractComposites(this);
+
+		__syncthreads();
+
+		//Final raking reduction of counts by bin, output to spine.
+
+		aggregate_counters.ShareCounters(this);
+
+		__syncthreads();
+
+		// Rake-reduce and write out the bin_count reductions
+		if (threadIdx.x < KernelPolicy::BINS) {
+
+			SizeT bin_count = util::reduction::SerialReduce<KernelPolicy::AGGREGATED_PARTIALS_PER_ROW>::Invoke(
+				smem_storage.aggregate[threadIdx.x]);
+
+			int spine_bin_offset = util::FastMul(gridDim.x, threadIdx.x) + blockIdx.x;
+
+			util::io::ModifiedStore<KernelPolicy::WRITE_MODIFIER>::St(
+					bin_count, d_spine + spine_bin_offset);
+		}
+	}
 };
 
 
