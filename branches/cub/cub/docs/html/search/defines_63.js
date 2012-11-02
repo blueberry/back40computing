e4c001a0fa622630d11d6dea91c3612d7eac8adc
duane.merrill <duane.merrill@f28d7702-48ea-1802-4fdf-2413024927d0>
2012-11-02 09:40:05 +0000

    Re-adding html docs

---
+++ branches/cub/cub/docs/html/search/defines_63.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['cubdebug',['CubDebug',['../debug_8cuh.html#a04236bb0db0efe7a19c9ecba0aedc1e5',1,'debug.cuh']]],
+  ['cubdebugexit',['CubDebugExit',['../debug_8cuh.html#a2e5de1db78fd84552bda8254efa409a3',1,'debug.cuh']]]
+];
---
+++ branches/cub/cub/docs/html/search/enumvalues_63.js
@@ -0,0 +1,11 @@
+var searchData=
+[
+  ['cta_5fload_5fdirect',['CTA_LOAD_DIRECT',['../namespacecub.html#ab659e51d8d1cb563d028fa55349ed27fa0504ed5bbda32afba910a5ccbf4b3c76',1,'cub']]],
+  ['cta_5fload_5ftranspose',['CTA_LOAD_TRANSPOSE',['../namespacecub.html#ab659e51d8d1cb563d028fa55349ed27fa130aac9bb12ce87e0ab0ec5dba4acfb8',1,'cub']]],
+  ['cta_5fload_5fvectorize',['CTA_LOAD_VECTORIZE',['../namespacecub.html#ab659e51d8d1cb563d028fa55349ed27fa14c41521144161ce388c42407b6aae19',1,'cub']]],
+  ['cta_5fscan_5fraking',['CTA_SCAN_RAKING',['../namespacecub.html#a9da6c6c170dadcf0700355c6291c7a7da0cbbf7621108c8c027f2be69c03406fa',1,'cub']]],
+  ['cta_5fscan_5fwarpscans',['CTA_SCAN_WARPSCANS',['../namespacecub.html#a9da6c6c170dadcf0700355c6291c7a7da451b2f8e1b7d3c9c4a1e83307e228ce5',1,'cub']]],
+  ['cta_5fstore_5fdirect',['CTA_STORE_DIRECT',['../namespacecub.html#a47ce001bb3545160ee2a725d9de40b9cac74f459f7d9e98b8bc7d76e3168c17b5',1,'cub']]],
+  ['cta_5fstore_5ftranspose',['CTA_STORE_TRANSPOSE',['../namespacecub.html#a47ce001bb3545160ee2a725d9de40b9ca991dcaff085113850fb90372ac9eb665',1,'cub']]],
+  ['cta_5fstore_5fvectorize',['CTA_STORE_VECTORIZE',['../namespacecub.html#a47ce001bb3545160ee2a725d9de40b9ca60250b5e6d978911bf61c6525b196c39',1,'cub']]]
+];
