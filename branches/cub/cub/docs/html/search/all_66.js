b29cd2a0278ca3dee60b4cb55ea9ff2a34a0ff96
duane.merrill <duane.merrill@f28d7702-48ea-1802-4fdf-2413024927d0>
2012-11-29 18:00:57 +0000

    remove html docs

--- branches/cub/cub/docs/html/search/all_66.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['flag',['Flag',['../classcub_1_1_cta_discontinuity.html#af98dfe71093fa7940d1843f1ea28bdc5',1,'cub::CtaDiscontinuity::Flag(SmemStorage &amp;smem_storage, T(&amp;input)[ITEMS_PER_THREAD], FlagOp flag_op, Flag(&amp;flags)[ITEMS_PER_THREAD], T &amp;last_tile_item)'],['../classcub_1_1_cta_discontinuity.html#ac55922455e0466b2dd45197d4a84b2b9',1,'cub::CtaDiscontinuity::Flag(SmemStorage &amp;smem_storage, T(&amp;input)[ITEMS_PER_THREAD], T tile_prefix, FlagOp flag_op, Flag(&amp;flags)[ITEMS_PER_THREAD], T &amp;last_tile_item)']]]
-];
