24bad31d1c1d80f2ab9d209b5bcd62ee4f1d9ec9
duane.merrill <duane.merrill@f28d7702-48ea-1802-4fdf-2413024927d0>
2012-11-29 16:18:37 +0000

    remove docs

--- branches/cub/cub/docs/html/search/all_6c.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['load',['Load',['../classcub_1_1_cta_load.html#a8acfa2517220d64d961524534d0fda94',1,'cub::CtaLoad::Load(SmemStorage &amp;smem_storage, T(&amp;items)[ITEMS_PER_THREAD], InputIterator itr, const SizeT &amp;cta_offset)'],['../classcub_1_1_cta_load.html#abfe8ce506b832c3013bdb81bd52ec409',1,'cub::CtaLoad::Load(SmemStorage &amp;smem_storage, T(&amp;items)[ITEMS_PER_THREAD], InputIterator itr, const SizeT &amp;cta_offset, const SizeT &amp;guarded_elements)']]]
-];
--- branches/cub/cub/docs/html/search/all_77.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['warp_5fscan',['warp_scan',['../structcub_1_1_warp_scan_1_1_warp_scan_internal_3_01_s_m_e_m___s_c_a_n_00_01_d_u_m_m_y_01_4_1_1_smem_storage.html#afc9fd6f60f6e5b785235e80b82e64692',1,'cub::WarpScan::WarpScanInternal&lt; SMEM_SCAN, DUMMY &gt;::SmemStorage']]],
-  ['warp_5fscan_2ecuh',['warp_scan.cuh',['../warp__scan_8cuh.html',1,'']]],
-  ['warpscan',['WarpScan',['../classcub_1_1_warp_scan.html',1,'cub']]]
-];
